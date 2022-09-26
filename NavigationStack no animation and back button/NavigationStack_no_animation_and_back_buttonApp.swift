2b16a92417d4cd14f2d7110b88a1c13a30f31d54
CarterH93 <Carterhawkins93@gmail.com>
2022-09-25 23:46:29 -0400

    Initial Commit

---
+++ NavigationStack no animation and back button/NavigationStack_no_animation_and_back_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack_no_animation_and_back_buttonApp.swift
+//  NavigationStack no animation and back button
+//
+//  Created by Carter Hawkins on 9/25/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack_no_animation_and_back_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
